a61e28cfc1ab220c0bc426f76bc3bba9d49c8ddf
Mathieu Ravaux <mathieu.ravaux@gmail.com>
2013-09-25 17:28:09 +0200

    Added a stateless config option to prevent the use of cookies.

--- lib/mini_profiler/profiler.rb
+++ lib/mini_profiler/profiler.rb
@@ -173,7 +173,7 @@ module Rack
 
     def call(env)
 
-      client_settings = ClientSettings.new(env)
+      client_settings = @config.stateless ? NullClientSettings : ClientSettings.new(env)
 
       status = headers = body = nil
       query_string = env['QUERY_STRING']
